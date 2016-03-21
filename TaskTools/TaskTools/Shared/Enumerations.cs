0ca73d8981e76db58c4f6ab9b66c27debe97c560
wordtinker <wordtinker@yandex.ru>
2016-03-21 23:22:41 +0300

    Fix enumerations

--- TaskTools/TaskTools/Shared/Enumerations.cs
+++ TaskTools/TaskTools/Shared/Enumerations.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.ComponentModel;
+using TaskTools.Properties;
 
 namespace TaskTools.Shared
 {
@@ -22,6 +24,7 @@ namespace TaskTools.Shared
         Today
     }
 
+    [TypeConverter(typeof(EnumDescriptionTypeConverter))]
     public enum RepeatType
     {
         LastDay = -1,
@@ -33,36 +36,67 @@ namespace TaskTools.Shared
         Friday = DayOfWeek.Friday, //5
         Saturday = DayOfWeek.Saturday, // 6
         Day = 7,
+        [LocalizedDescription("_1", typeof(Resources))]
         _1 = 101,
+        [LocalizedDescription("_2", typeof(Resources))]
         _2 = 102,
+        [LocalizedDescription("_3", typeof(Resources))]
         _3 = 103,
+        [LocalizedDescription("_4", typeof(Resources))]
         _4 = 104,
+        [LocalizedDescription("_5", typeof(Resources))]
         _5 = 105,
+        [LocalizedDescription("_6", typeof(Resources))]
         _6 = 106,
+        [LocalizedDescription("_7", typeof(Resources))]
         _7 = 107,
+        [LocalizedDescription("_8", typeof(Resources))]
         _8 = 108,
+        [LocalizedDescription("_9", typeof(Resources))]
         _9 = 109,
+        [LocalizedDescription("_10", typeof(Resources))]
         _10 = 110,
+        [LocalizedDescription("_11", typeof(Resources))]
         _11 = 111,
+        [LocalizedDescription("_12", typeof(Resources))]
         _12 = 112,
+        [LocalizedDescription("_13", typeof(Resources))]
         _13 = 113,
+        [LocalizedDescription("_14", typeof(Resources))]
         _14 = 114,
+        [LocalizedDescription("_15", typeof(Resources))]
         _15 = 115,
+        [LocalizedDescription("_16", typeof(Resources))]
         _16 = 116,
+        [LocalizedDescription("_17", typeof(Resources))]
         _17 = 117,
+        [LocalizedDescription("_18", typeof(Resources))]
         _18 = 118,
+        [LocalizedDescription("_19", typeof(Resources))]
         _19 = 119,
+        [LocalizedDescription("_20", typeof(Resources))]
         _20 = 120,
+        [LocalizedDescription("_21", typeof(Resources))]
         _21 = 121,
+        [LocalizedDescription("_22", typeof(Resources))]
         _22 = 122,
+        [LocalizedDescription("_23", typeof(Resources))]
         _23 = 123,
+        [LocalizedDescription("_24", typeof(Resources))]
         _24 = 124,
+        [LocalizedDescription("_25", typeof(Resources))]
         _25 = 125,
+        [LocalizedDescription("_26", typeof(Resources))]
         _26 = 126,
+        [LocalizedDescription("_27", typeof(Resources))]
         _27 = 127,
+        [LocalizedDescription("_28", typeof(Resources))]
         _28 = 128,
+        [LocalizedDescription("_29", typeof(Resources))]
         _29 = 129,
+        [LocalizedDescription("_30", typeof(Resources))]
         _30 = 130,
+        [LocalizedDescription("_31", typeof(Resources))]
         _31 = 131
     }
 }
